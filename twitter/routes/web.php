97ef89136202e999b33689ae8eba982f021285d2
河野康平 <kawanokohei@HajimariKoheiKawano.local>
2023-10-27 19:40:37 +0900

    :update:ようちゃんさんのコメント修正(やり残し)

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TweetController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TweetController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
