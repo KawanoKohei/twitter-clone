b37ca5df520d1e6aaa9fb08c6d9b364563410f26
河野康平 <kawanokohei@HajimariKoheiKawano.local>
2023-10-30 20:10:20 +0900

    :update:useの並び順変更

--- twitter/app/Models/User.php
+++ twitter/app/Models/User.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use App\Models\Follower;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
